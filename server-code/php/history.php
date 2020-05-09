926a683f5dd4cef1c3542b3e75646fc8fff2ff09
Kidwithservers <50305191+Kidwithservers@users.noreply.github.com>
2020-05-09 17:29:25 -0400

    fixed php for history feature

--- server-code/php/history.php
+++ server-code/php/history.php
@@ -1,19 +1,27 @@
 <?php
+//db connection
 include "db.php";
-$name=$_POST['name'];
-$rows=array();
-$query = $con->prepare("SELECT * FROM `main_data` WHERE `name` = '?' ORDER BY `date` DESC;");
+
+//get data
+$name =$_GET['name'];
+
+//query setup and execution
+$query = $con->prepare("SELECT `steps`, `date` FROM `main_data` WHERE `name` = ? ORDER BY `date` DESC;");
 $query->bind_param("s", $name);
 $query->execute();
-if (mysqli_num_rows($q) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($q)) {
-      // echo $row["name"] . $row["steps"]. $row["date"];
-      $rows[] = $row;
-}
-print json_encode($rows);
 
-} else {
-    echo "0 results";
+//data storage
+$query->bind_result($data["steps"], $data["date"]);
+while ($query->fetch()) {
+    $row = [];
+    foreach ($data as $key => $val) {
+        $row[$key] = $val;
+    }
+    $array[] = $row;
 }
+
+//echo data and end query
+echo json_encode($array);
+$query->close();
+
 ?>
